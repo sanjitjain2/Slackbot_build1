53ef4b632dca0ee9df3a9cd7298dc0d52bdb6f2b
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2017-08-15 12:37:24 +0530

    formatted

--- lang_translator.py
+++ lang_translator.py
@@ -1,6 +1,7 @@
 from googletrans import Translator
 
 def translate(message):
+    
     translator = Translator( service_urls=[
           'translate.google.com',
           'translate.google.co.kr',
@@ -9,14 +10,15 @@ def translate(message):
     msg_arr = message.split()
     script = ' '.join(msg_arr[2:])
     lang = msg_arr[1]
+    
     translations = translator.translate([script], dest=lang.replace('-', ''))
 
     for translation in translations:
         return 'Translated script is: \n' + translation.text
-        #print(translation.origin, ' -> ', translation.text)
 
 '''
 Language Codes:
+
 Dutch: nl
 French: fr
 Spanish: de
